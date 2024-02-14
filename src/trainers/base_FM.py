32c7024ab356a00f9762deedd2286ffd4b67f98f
Wu Wang Yang <56248896+wuwangyang24@users.noreply.github.com>
2024-02-14 22:02:56 +0100

    Update base_FM.py

--- src/trainers/base_FM.py
+++ src/trainers/base_FM.py
@@ -15,32 +15,38 @@ from src.networks import PassthroughVQVAE
 from src.utils.simplex_noise import Simplex_CLASS
 
 import wandb
+from accelerate import Accelerator
 
 
 class BaseTrainerFM:
     def __init__(self, args):
 
         # initialise DDP if run was launched with torchrun
-        if "LOCAL_RANK" in os.environ:
-            print("Setting up DDP.")
-            self.ddp = True
-            # disable logging for processes except 0 on every node
-            local_rank = int(os.environ["LOCAL_RANK"])
-            if local_rank != 0:
-                # f = open(os.devnull, "w")
-                # sys.stdout = sys.stderr = f
-                pass
-            # else:
-            #     wandb.login(key=args.wandb.key)
-            #     self.run = wandb.init(entity=args.wandb.entity, project=args.wandb.project)
+        # if "LOCAL_RANK" in os.environ:
+        #     print("Setting up DDP.")
+        #     self.ddp = True
+        #     # disable logging for processes except 0 on every node
+        #     local_rank = int(os.environ["LOCAL_RANK"])
+        #     # if local_rank != 0:
+        #     #     f = open(os.devnull, "w")
+        #     #     sys.stdout = sys.stderr = f
+        #     wandb.login(key=args.wandb.key)
+        #     self.run = wandb.init(entity=args.wandb.entity, 
+        #                           project=args.wandb.project,
+        #                           group='DDP'
+        #                          )
 
-            # initialize the distributed training process, every GPU runs in a process
-            dist.init_process_group(backend="nccl", init_method="env://")
-            self.device = torch.device(f"cuda:{local_rank}")
-        else:
-            self.ddp = False
-            self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        torch.cuda.set_device(self.device)
+        #     # initialize the distributed training process, every GPU runs in a process
+        #     dist.init_process_group(backend="nccl", init_method="env://")
+        #     self.device = torch.device(f"cuda:{local_rank}")
+        # else:
+        #     self.ddp = False
+        #     self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        # torch.cuda.set_device(self.device)
+        
+        self.accelerator = Accelerator()
+        self.device = self.accelerator.device
+        # torch.cuda.set_device(self.device)
 
         print(f"Arguments: {str(args)}")
         for k, v in vars(args).items():
@@ -71,6 +77,7 @@ class BaseTrainerFM:
             ).to(self.device)
         else:
             raise ValueError(f"Do not recognise model type {args.model_type}")
+
         print(f"{sum(p.numel() for p in self.model.parameters()):,} model parameters")
 
         self.scaler = GradScaler()
@@ -98,30 +105,42 @@ class BaseTrainerFM:
             self.best_loss = 1000
             self.global_step = 0
             self.found_checkpoint = False
-
+            
+        self.model = self.accelerator.prepare(self.model) #use accelarater for ddp
         self.optimizer = torch.optim.Adam(params=self.model.parameters(), lr=args.train.lr)
         if checkpoint_path.exists():
             self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
 
         # wrap the model with DistributedDataParallel module
-        if self.ddp:
-            self.model = DistributedDataParallel(
-                self.model, device_ids=[self.device], find_unused_parameters=True
-            )
+        # if self.ddp:
+        #     self.model = DistributedDataParallel(
+        #         self.model, device_ids=[self.device], find_unused_parameters=True
+        #     )
+        self.optimizer = self.accelerator.prepare(self.optimizer) #use accelarater for ddp
 
     def save_checkpoint(self, path, epoch, save_message=None):
-        if self.ddp and dist.get_rank() == 0:
-            # if DDP save a state dict that can be loaded by non-parallel models
-            checkpoint = {
-                "epoch": epoch + 1,  # save epoch+1, so we resume on the next epoch
-                "global_step": self.global_step,
-                "model_state_dict": self.model.module.state_dict(),
-                "optimizer_state_dict": self.optimizer.state_dict(),
-                "best_loss": self.best_loss,
-            }
-            print(save_message)
-            torch.save(checkpoint, path)
-        if not self.ddp:
+        # if self.ddp and dist.get_rank() == 0:
+        #     # if DDP save a state dict that can be loaded by non-parallel models
+        #     checkpoint = {
+        #         "epoch": epoch + 1,  # save epoch+1, so we resume on the next epoch
+        #         "global_step": self.global_step,
+        #         "model_state_dict": self.model.module.state_dict(),
+        #         "optimizer_state_dict": self.optimizer.state_dict(),
+        #         "best_loss": self.best_loss,
+        #     }
+        #     print(save_message)
+        #     torch.save(checkpoint, path)
+        # if not self.ddp:
+        #     checkpoint = {
+        #         "epoch": epoch + 1,  # save epoch+1, so we resume on the next epoch
+        #         "global_step": self.global_step,
+        #         "model_state_dict": self.model.state_dict(),
+        #         "optimizer_state_dict": self.optimizer.state_dict(),
+        #         "best_loss": self.best_loss,
+        #     }
+        #     print(save_message)
+        #     torch.save(checkpoint, path)
+        if self.accelerator.is_main_process:
             checkpoint = {
                 "epoch": epoch + 1,  # save epoch+1, so we resume on the next epoch
                 "global_step": self.global_step,
@@ -131,3 +150,4 @@ class BaseTrainerFM:
             }
             print(save_message)
             torch.save(checkpoint, path)
+        self.accelerator.wait_for_everyone()
